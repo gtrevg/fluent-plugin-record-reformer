e80fef6b2f6e7ce15942187dfec5c13bb81e458f
Naotoshi Seo <sonots@gmail.com>
2014-01-20 23:39:20 +0900

    cleanup

--- lib/fluent/plugin/out_record_reformer.rb
+++ lib/fluent/plugin/out_record_reformer.rb
@@ -36,26 +36,25 @@ module Fluent
         @remove_keys = @remove_keys.split(',')
       end
 
+      @hostname = Socket.gethostname
+
       @placeholder_expander =
         if @enable_ruby
           # require utilities which would be used in ruby placeholders
           require 'pathname'
           require 'uri'
           require 'cgi'
-          RubyPlaceholderExpander.new
+          RubyPlaceholderExpander.new(:hostname => @hostname)
         else
-          PlaceholderExpander.new
+          PlaceholderExpander.new(:hostname => @hostname)
         end
-
-      @hostname = Socket.gethostname
     end
 
     def emit(tag, es, chain)
-      tag_parts = tag.split('.')
-      tag_prefix = tag_prefix(tag_parts)
-      tag_suffix = tag_suffix(tag_parts)
+      @placeholder_expander.prepare_tag_placeholders(tag)
       es.each { |time, record|
-        new_tag, new_record = reform(@output_tag, record, tag, tag_parts, tag_prefix, tag_suffix, Time.at(time))
+        @placeholder_expander.prepare_event_placeholders(time, record)
+        new_tag, new_record = reform(@output_tag, record)
         Engine.emit(new_tag, time, new_record)
       }
       chain.next
@@ -65,8 +64,7 @@ module Fluent
 
     private
 
-    def reform(output_tag, record, tag, tag_parts, tag_prefix, tag_suffix, time)
-      @placeholder_expander.prepare_placeholders(record, tag, tag_parts, tag_prefix, tag_suffix, @hostname, time)
+    def reform(output_tag, record)
       new_tag = @placeholder_expander.expand(output_tag)
 
       new_record = @renew_record ? {} : record.dup
@@ -76,34 +74,43 @@ module Fluent
       [new_tag, new_record]
     end
 
-    def tag_prefix(tag_parts)
-      return [] if tag_parts.empty?
-      tag_prefix = [tag_parts.first]
-      1.upto(tag_parts.size-1).each do |i|
-        tag_prefix[i] = "#{tag_prefix[i-1]}.#{tag_parts[i]}"
+    module TagHelper
+      def tag_prefix(tag_parts)
+        return [] if tag_parts.empty?
+        tag_prefix = [tag_parts.first]
+        1.upto(tag_parts.size-1).each do |i|
+          tag_prefix[i] = "#{tag_prefix[i-1]}.#{tag_parts[i]}"
+        end
+        tag_prefix
       end
-      tag_prefix
-    end
 
-    def tag_suffix(tag_parts)
-      return [] if tag_parts.empty?
-      rev_tag_parts = tag_parts.reverse
-      rev_tag_suffix = [rev_tag_parts.first]
-      1.upto(tag_parts.size-1).each do |i|
-        rev_tag_suffix[i] = "#{rev_tag_parts[i]}.#{rev_tag_suffix[i-1]}"
+      def tag_suffix(tag_parts)
+        return [] if tag_parts.empty?
+        rev_tag_parts = tag_parts.reverse
+        rev_tag_suffix = [rev_tag_parts.first]
+        1.upto(tag_parts.size-1).each do |i|
+          rev_tag_suffix[i] = "#{rev_tag_parts[i]}.#{rev_tag_suffix[i-1]}"
+        end
+        rev_tag_suffix.reverse
       end
-      rev_tag_suffix.reverse
     end
 
     class PlaceholderExpander
-      # referenced https://github.com/fluent/fluent-plugin-rewrite-tag-filter, thanks!
+      include TagHelper
       attr_reader :placeholders
 
-      def prepare_placeholders(record, tag, tag_parts, tag_prefix, tag_suffix, hostname, time)
+      def initialize(hash)
+        @placeholders = {}
+        hash.each { |k, v| @placeholders.store("${#{k}}", v) }
+      end
+
+      def prepare_tag_placeholders(tag)
+        tag_parts  = tag.split('.')
+        tag_prefix = tag_prefix(tag_parts)
+        tag_suffix = tag_suffix(tag_parts)
+
         placeholders = {
-          '${time}' => time.to_s,
           '${tag}' => tag,
-          '${hostname}' => hostname,
         }
 
         size = tag_parts.size
@@ -127,11 +134,19 @@ module Fluent
           placeholders.store("${tag_suffix[#{idx-size}]}", t) # support tag_suffix[-1]
         }
 
+        @placeholders.merge!(placeholders)
+      end
+
+      def prepare_event_placeholders(time, record)
+        placeholders = {
+          '${time}' => Time.at(time).to_s,
+        }
+
         record.each { |k, v|
           placeholders.store("${#{k}}", v)
         }
 
-        @placeholders = placeholders
+        @placeholders.merge!(placeholders)
       end
 
       def expand(str)
@@ -143,26 +158,33 @@ module Fluent
     end
 
     class RubyPlaceholderExpander
+      include TagHelper
       attr_reader :placeholders
+
+      def initialize(hash)
+        @placeholders = UndefOpenStruct.new(hash)
+      end
 
       # Get placeholders as a struct
       #
-      # @param [Hash]   record      the record, one of information
       # @param [String] tag         the tag
-      # @param [Array]  tag_parts   the tag parts (tag splitted by .)
-      # @param [Array]  tag_prefix  the tag prefix parts
-      # @param [Array]  tag_suffix  the tag suffix parts
-      # @param [String] hostname    the hostname
+      def prepare_tag_placeholders(tag)
+        tag_parts  = tag.split('.')
+        tag_prefix = tag_prefix(tag_parts)
+        tag_suffix = tag_suffix(tag_parts)
+        @placeholders.tag  = tag
+        @placeholders.tags = @placeholders.tag_parts = tag_parts # tags is for old version compatibility
+        @placeholders.tag_prefix = tag_prefix
+        @placeholders.tag_suffix = tag_suffix
+      end
+
+      # Get event stream (time, record) placeholders as a struct
+      #
       # @param [Time]   time        the time
-      def prepare_placeholders(record, tag, tag_parts, tag_prefix, tag_suffix, hostname, time)
-        struct = UndefOpenStruct.new(record)
-        struct.tag  = tag
-        struct.tags = struct.tag_parts = tag_parts # tags is for old version compatibility
-        struct.tag_prefix = tag_prefix
-        struct.tag_suffix = tag_suffix
-        struct.time = time
-        struct.hostname = hostname
-        @placeholders = struct
+      # @param [Hash]   record      the record
+      def prepare_event_placeholders(time, record)
+        @placeholders.time = Time.at(time)
+        record.each { |k, v| @placeholders.__send__("#{k}=", v) }
       end
 
       # Replace placeholders in a string
